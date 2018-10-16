c2e8add81e13850a7043288cfb225ebd5898835b
David Orme <david.orme@rallyhealth.com>
2018-10-15 21:05:56 -0500

    Call start-router at app start

--- src/clj/fusecode/server/random.clj
+++ src/clj/fusecode/server/random.clj
@@ -1,7 +1,9 @@
 (ns fusecode.server.random
   (:require [fusecode.server.handler :as handler]))
 
-(def app handler/routes)
+(defn app [] 
+  (handler/start-router!)
+  (handler/routes))
 
 (defmethod handler/event-msg-handler :simple/get-random
   [{:as ev-msg :keys [event id ?data ring-req ?reply-fn send-fn]}]
